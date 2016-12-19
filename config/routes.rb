73866d29ca74ff0e14df206a2c7b287e0ebbc3f3
dangerstephen <dangersteve1@gmail.com>
2016-12-19 15:07:41 -0800

    added auth and flash messages to tell if there was an error or success

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#index'
+
+  get '/' => 'users#index'
+  get '/signup' => 'users#new', as: 'new_user'
+  post '/users' => 'users#create'
+  get '/users/:id' => 'users#show', as: 'user'
+  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
+  post '/sessions' => 'sessions#create'
+
 end
